0bef48944c4bebac603ebee3978150026e62c482
Keithkiarie <keithkiarie@gmail.com>
2020-08-14 15:14:19 +0300

    template

--- Simulator/Buyer.js
+++ Simulator/Buyer.js
@@ -1,6 +1,22 @@
 class Buyer {
 
+    MaximumPay;
+
     constructor() {
-        
+        this.MaximumPay = this.GetRandomPrice()
+    }
+
+    AdjustPrice(SuccessfulPurchase) {
+        if (SuccessfulPurchase) {
+            // price adjusted downwards if the previous transaction was successful
+
+        } else {
+            // price adjusted upwards if the previous transaction was not successful
+
+        }
+    }
+
+    static GetRandomPrice() {
+        return Math.random() * 100;
     }
 }
--- Simulator/Seller.js
+++ Simulator/Seller.js
@@ -1,6 +1,22 @@
 class Seller {
 
+    MinimumPrice;
+
     constructor() {
-        
+        this.MinimumPrice = this.GetRandomPrice()
+    }
+
+    AdjustPrice(SuccessfulSale) {
+        if (SuccessfulSale) {
+            // price adjusted downwards if the previous transaction was successful
+
+        } else {
+            // price adjusted upwards if the previous transaction was not successful
+
+        }
+    }
+
+    static GetRandomPrice() {
+        return (Math.random() * 99) + 1;
     }
 }
